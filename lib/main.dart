b59405f04b754833c1e09ba5f06b663b7e71d0e5
rashidminhas <rashidminhassheikh47@gmail.com>
2025-09-25 00:57:11 +0500

    app version updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hostel_management/decider_screen.dart';
-import 'package:hostel_management/features/authentication/screens/signup/sign_up_student.dart';
-import 'package:hostel_management/features/authentication/screens/signup/verify_email.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
-void main() async{
 
-WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
 
   runApp(App());
-  
 }
 
 class App extends StatelessWidget {
@@ -21,8 +16,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      home: DeciderScreen(),
-    );
+    return GetMaterialApp(home: DeciderScreen());
   }
 }
